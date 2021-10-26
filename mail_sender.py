638cd23ac249f46e3ba761806d4629f13d1c2239
Pietro Zarri <pietrozarri96@gmail.com>
2021-10-26 18:05:23 +0200

    refactoring and better error handling

--- mail_sender.py
+++ mail_sender.py
@@ -31,10 +31,8 @@ class MailSender:
     def sent_pdf_list_to_customer(self, code, customer_email, mail_type, pdf_list):
         try:
             # check mail type
-            if mail_type == "Falso":
-                self._send_mail(customer_email, pdf_list)
-            elif mail_type == "Vero":
-                self._send_mail_pec(customer_email, pdf_list)
+            if mail_type == "Falso" or mail_type == "Vero":
+                self._send_mail(customer_email, mail_type, pdf_list)
             else:
                 raise ValueError("Can't read mail type value!")
             # Move pdf file to "sent" folder
@@ -44,49 +42,20 @@ class MailSender:
                             " for client " + code+"\n")
 
     # send mail with standard mail address
-    def _send_mail(self, receiver, pdf_list):
+    def _send_mail(self, receiver, mail_type, pdf_list):
         logging.info("sending standard mail to: " + receiver)
 
         body = utils.read_mail_content()
 
-        message = MIMEMultipart()
-        message['From'] = self.sender
-        message['To'] = receiver
-        message['Subject'] = utils.read_subject_content() + ' ' + self._month + ' ' + self._year
-
+        sender_by_mail_type = self._get_sender_by_mail_type(mail_type)
+        message = self._build_message(receiver, sender_by_mail_type)
         message.attach(MIMEText(body, 'plain'))
-
         message = self._add_attachments(message, pdf_list)
 
-        server = smtplib.SMTP_SSL(self.smtp_ssl_host, self.smtp_ssl_port)
-        server.login(self.username, self.password)
+        server = self._build_server(mail_type)
 
         text = message.as_string()
-        server.sendmail(self.sender, receiver, text)
-        server.quit()
-
-        logging.info("mail inviata!\n")
-
-    # send mail with pec address
-    def _send_mail_pec(self, receiver, pdf_list):
-        logging.info("sending pec mail to: " + receiver)
-
-        body = utils.read_mail_content()
-
-        message = MIMEMultipart()
-        message['From'] = self.sender_pec
-        message['To'] = receiver
-        message['Subject'] = utils.read_subject_content() + ' ' + self._month + ' ' + self._year
-
-        message.attach(MIMEText(body, 'plain'))
-
-        message = self._add_attachments(message, pdf_list)
-
-        server = smtplib.SMTP_SSL(self.smtp_ssl_host_pec, self.smtp_ssl_port_pec)
-        server.login(self.username_pec, self.password_pec)
-
-        text = message.as_string()
-        server.sendmail(self.sender_pec, receiver, text)
+        server.sendmail(sender_by_mail_type, receiver, text)
         server.quit()
 
         logging.info("mail sent!\n")
@@ -113,6 +82,44 @@ class MailSender:
             message.attach(payload)
         logging.info(attachments_list)
         return message
+
+    def _get_sender_by_mail_type(self, mail_type):
+        if mail_type == "False":
+            sender_by_mail_type = self.sender
+        else:
+            sender_by_mail_type = self.sender_pec
+        return sender_by_mail_type
+
+    def _build_message(self, receiver, sender):
+        message = MIMEMultipart()
+        message['From'] = sender
+        message['To'] = receiver
+        message['Subject'] = utils.read_subject_content() + ' ' + self._month + ' ' + self._year
+        return message
+
+    def _build_server(self, mail_type):
+        if mail_type == "Falso":
+            host = self.smtp_ssl_host
+            port = self.smtp_ssl_port
+            username = self.username
+            psw = self.password
+        else:
+            host = self.smtp_ssl_host_pec
+            port = self.smtp_ssl_port_pec
+            username = self.username_pec
+            psw = self.password_pec
+        try:
+            server = smtplib.SMTP_SSL(host, port)
+            server.login(username, psw)
+        except smtplib.SMTPAuthenticationError:
+            logging.error("Email authentication failed!\n")
+            utils.log_end_execution()
+            quit()
+        except Exception:
+            logging.error("Error connecting to email server, check internet connection or smtp parameters!\n")
+            utils.log_end_execution()
+            quit()
+        return server
 
     def set_month(self, month):
         self._month = month
