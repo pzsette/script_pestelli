37d34c49c63610ef50a49eae7a88fedaee07442e
Pietro Zarri <pietrozarri96@gmail.com>
2021-10-27 16:09:22 +0200

    add check main folder

--- path_utils.py
+++ path_utils.py
@@ -1,22 +1,22 @@
-import configparser
 import os
 import utils
 import logging
 
 
+def get_file_folder():
+    return utils.read_config_value('PATH', 'file_folder')
+
+
 def get_to_send_folder_path():
-    return os.path.join(utils.read_config_value('PATH', 'file_folder'),
-                        utils.read_config_value('PATH', 'to_send_folder'))
+    return os.path.join(get_file_folder(), utils.read_config_value('PATH', 'to_send_folder'))
 
 
 def get_sent_folder_path():
-    return os.path.join(utils.read_config_value('PATH', 'file_folder'),
-                        utils.read_config_value('PATH', 'sent_folder'))
+    return os.path.join(get_file_folder(), utils.read_config_value('PATH', 'sent_folder'))
 
 
 def get_data_path():
-    return os.path.join(utils.read_config_value('PATH', 'file_folder'),
-                        utils.read_config_value('PATH', 'data_folder'))
+    return os.path.join(get_file_folder(), utils.read_config_value('PATH', 'data_folder'))
 
 
 def get_clients_file():
@@ -32,6 +32,9 @@ def get_email_subject():
 
 
 def check_if_all_paths_exist():
+    if not os.path.isdir(get_data_path()):
+        logging.error("Can't find data folder")
+        utils.end_execution()
     if not os.path.isdir(get_to_send_folder_path()):
         logging.error("Can't find to send folder")
         utils.end_execution()
